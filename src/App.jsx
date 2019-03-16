1da952e68e50ac564c666bb225ac56276e8a774d
FX-Wood <fxwood2@gmail.com>
2019-03-16 12:13:12 -0700

    Finished app skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -36,6 +36,11 @@ class App extends Component {
           <h2>Issue number</h2>
           <p>open status indicator <span><em>amount of time issue has been open</em></span></p>
           <hr/>
+          <img src="http://via.placeholderimg/80" alt="user image"/>
+          <div>
+            <h2>Parsed markdown</h2>
+            <p>asdklfjl;kasdjfl;kasdjflkasjdf asdjlfkasj dlkfj aslkdf jaslkdjf alksdjfasdj flk;asdj fals;djf l;aksdjf laksdjfhiuqeo;fjasd;klf jasf iqae jflask;jdf ;asdfj ak;sdjhfkjasdhglasdfj asdf lkasjf sak</p>
+          </div>
         </div>
       </div>
     );
