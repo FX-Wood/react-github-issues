f5aedecfc8724341339513f19b2445dafa7a8695
FX-Wood <fxwood2@gmail.com>
2019-03-16 12:31:38 -0700

    Abstracted splash page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import './App.css';
+import SplashPage from './SplashPage';
 
 class App extends Component {
   constructor(props) {
@@ -31,11 +32,7 @@ class App extends Component {
             <li>Explore</li>
           </ul>
         </nav>
-        <div className="splash">
-          <h2>Want to submit an issue to bookface/actre</h2>
-          <p>If you have a bug or an idea, read the contributing guidelines before opening an issue. Issues labeled good first issue can be good first contributions</p>
-          <button>Go to Issues</button>
-        </div>
+        <SplashPage />
         <div className="index">
           <h2>Number of issues</h2>
           <ul>
---
+++ src/SplashPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function SplashPage(props) {
+    return(
+        <div className="splash">
+            <h2>Want to submit an issue to bookface/react?</h2>
+            <p>If you have a bug or an idea, read the contributing guidelines before opening an issue. Issues labeled good first issue can be good first contributions</p>
+            <button>Go to Issues</button>
+      </div>
+    )
+}
